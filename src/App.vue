f5944b0ec0227e61bb4899a83c857003fb707199
zhongqi96 <zhongqi96@users.noreply.github.com>
2023-10-10 10:51:09 +0000

    Initial release

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-model="msg">
+    <p>msg: {{ msg }}</p>
+    <button @click="handelHello">hello</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  @Component
+  export default class App extends Vue {
+    // 初始化数据
+    msg: string = 'hello word'
+
+    mounted () {
+    }
+
+    // 方法
+    handelHello () {
+      this.msg = 'haha~~~ hello word!'
+    }
+  }
+</script>
